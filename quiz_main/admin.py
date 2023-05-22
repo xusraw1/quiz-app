fd7020d1c3f316da2e73e826a320caf95a5c3e54
Xusrav <xusrav200329@gmail.com>
2023-05-22 21:45:01 +0500

    Подключил модели к админ панели, для того чтобы можно было редактировать их из админки

--- quiz_main/admin.py
+++ quiz_main/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Category, Question, Test
+
 
 # Register your models here.
+
+class QuestionInline(admin.TabularInline):
+    model = Question
+
+
+class TestAdmin(admin.ModelAdmin):
+    inlines = [QuestionInline]
+    list_display = ['title', 'author']
+
+
+admin.site.register(Question)
+admin.site.register(Category)
+admin.site.register(Test, TestAdmin)
