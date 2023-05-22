32abe8fdf95ff0480fc93f7a5c38b43c9f6aa220
Xusrav <xusrav200329@gmail.com>
2023-05-22 21:36:28 +0500

    Создал вюху для регистрации пользователя, пока что он дефолтно принимает юсернейм и пароль, в дальнейшем хочу добавить поля возвраста, телефона номера и email, причина того что я не добавил сейчас, хочу попрактиковать изменить существующий код в GitHub, ну и создал шаблон для регистрации и связал с url вюху

--- quiz_main/views.py
+++ quiz_main/views.py
@@ -1,4 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+
+
+def sign_up(request):
+    form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+    return render(request, 'registration/sign_up.html', {'form': form})
 
 
 def index(request):
