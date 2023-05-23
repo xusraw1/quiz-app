6f2e9d5faadfb5142ce14287eac6a48e9919b48b
Xusrav <xusrav200329@gmail.com>
2023-05-23 14:49:20 +0500

    Создал вюв для решения теста и шаблон для него

--- quiz_main/urls.py
+++ quiz_main/urls.py
@@ -4,5 +4,7 @@ from .views import *
 urlpatterns = [
     path('', index, name='index'),
     path('signup/', sign_up, name='signup'),
-    path('ready_to_test/<int:pk>/', ready_to_test, name='ready_to_test')
+    path('ready_to_test/<int:pk>/', ready_to_test, name='ready_to_test'),
+    path('test/<int:pk>/', test, name='test'),
+
 ]
--- quiz_main/views.py
+++ quiz_main/views.py
@@ -6,6 +6,26 @@ from django.contrib.auth.decorators import login_required
 
 
 @login_required(login_url='login')
+def test(request, pk):
+    test = get_object_or_404(Test, id=pk)
+    questions = Question.objects.filter(test=test)
+    if request.method == 'POST':
+        for question in questions:
+            given_answer = request.POST[str(question.id)]
+            print(given_answer)
+            if question.T == given_answer:
+                print(True)
+            else:
+                print(False)
+    context = {
+        'question': questions,
+        'test': test,
+
+    }
+    return render(request, 'test.html', context)
+
+
+@login_required(login_url='login')
 def ready_to_test(request, pk):
     test = get_object_or_404(Test, id=pk)
     return render(request, 'ready_to_test.html', {'test': test})
